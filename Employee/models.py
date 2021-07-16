d2c3b153d2b60f0e10af2d5a9b64694683f0afe9
paradocx96 <adilanga@gmail.com>
2021-07-16 21:29:05 +0530

    Employee Model Added

--- Employee/models.py
+++ Employee/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
+    create_date = models.DateTimeField(default=now, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'employees'
+
+    def __str__(self):
+        return self.first_name
