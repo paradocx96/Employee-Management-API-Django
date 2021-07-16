2c6a0bb869c3e52507e397e44a269f6e997c4e4e
paradocx96 <adilanga@gmail.com>
2021-07-16 21:59:42 +0530

    Employee Serializer REST Functions Added

--- Employee/views.py
+++ Employee/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Employee
+from .serializers import EmployeeSerializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def employeeList(request):
+    employees = Employee.objects.all()
+    serializer = EmployeeSerializer(employees, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def employeeById(request, pk):
+    employee = Employee.objects.get(id=pk)
+    serializer = EmployeeSerializer(employee, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def employeeCreate(request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def employeeUpdate(request, pk):
+    employee = Employee.objects.get(id=pk)
+    serializer = EmployeeSerializer(instance=employee, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def employeeDelete(request, pk):
+    employee = Employee.objects.get(id=pk)
+    employee.delete()
+    return Response('Deleted!')
